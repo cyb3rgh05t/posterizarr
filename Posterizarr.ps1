2b466ba928f7f3c82c28771039a3bece09ec8798
FSCorrupt <45659314+fscorrupt@users.noreply.github.com>
2025-11-06 10:39:00 +0100

    Bump version to 2.1.0
    Updated script and release version numbers from 2.0.13 to 2.1.0 to reflect the new release.

--- Posterizarr.ps1
+++ Posterizarr.ps1
@@ -52,7 +52,7 @@ for ($i = 0; $i -lt $ExtraArgs.Count; $i++) {
     }
 }
 
-$CurrentScriptVersion = "2.0.13"
+$CurrentScriptVersion = "2.1.0"
 $global:HeaderWritten = $false
 $ProgressPreference = 'SilentlyContinue'
 $env:PSMODULE_ANALYSIS_CACHE_PATH = $null
