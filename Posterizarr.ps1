c87a884a60f2217cce65cdf92a5c90b428b22883
FSCorrupt <45659314+fscorrupt@users.noreply.github.com>
2025-11-21 05:53:36 +0100

    Fix missing assets in gallery.

--- Posterizarr.ps1
+++ Posterizarr.ps1
@@ -53,7 +53,7 @@ for ($i = 0; $i -lt $ExtraArgs.Count; $i++) {
     }
 }
 
-$CurrentScriptVersion = "2.1.14"
+$CurrentScriptVersion = "2.1.15"
 $global:HeaderWritten = $false
 $ProgressPreference = 'SilentlyContinue'
 $env:PSMODULE_ANALYSIS_CACHE_PATH = $null
