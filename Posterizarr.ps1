835e0d044fe5b8535f55fb8195c582fa22eed0ee
FSCorrupt <45659314+fscorrupt@users.noreply.github.com>
2025-12-12 12:57:20 +0100

    Version Bump

--- Posterizarr.ps1
+++ Posterizarr.ps1
@@ -53,7 +53,7 @@ for ($i = 0; $i -lt $ExtraArgs.Count; $i++) {
     }
 }
 
-$CurrentScriptVersion = "2.2.3"
+$CurrentScriptVersion = "2.2.4"
 $global:HeaderWritten = $false
 $ProgressPreference = 'SilentlyContinue'
 $env:PSMODULE_ANALYSIS_CACHE_PATH = $null
